fdb5ca0ff9f09babe50f1283c8d001ece1455991
Fikutaro <nomio9029@gmail.com>
2021-07-16 22:27:16 +0900

    practice

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "おほげ";
+		System.out.println(str);
+		 str = "おふが";
+		System.out.println(str);
+	    System.out.println(LUCKY_NUMBER);
+	}
+}
