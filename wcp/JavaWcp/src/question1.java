ea0295daf9cbf52828e52b0f31680f0a01d69216
Fikutaro <nomio9029@gmail.com>
2021-07-25 17:26:50 +0900

    question1

---
+++ wcp/JavaWcp/src/question1.java
@@ -0,0 +1,18 @@
+
+public class question1 {
+	public static void main(String[] args) {
+		int x = 20;
+		int y = x / 3;
+		int z = x % 3;
+		System.out.println(y);
+		System.out.println(z);
+		
+		x+= 2;
+		System.out.println(x);
+		x--;
+		System.out.println(x);
+		
+		double r = Math.pow(x, 2);
+		System.out.println(r);
+	}
+}
