5c9f6e9210b2828778d1e3622c4dfc7775c30486
Fikutaro <nomio9029@gmail.com>
2021-07-30 23:20:03 +0900

    practice fibonacci

--- wcp/JavaWcp/src/fibonacci.java
+++ wcp/JavaWcp/src/fibonacci.java
@@ -1,15 +1,14 @@
 
 public class fibonacci {
 	public static void main(String[] args) {
-		int x = 0;
-		int y = 1;
-		int z = x + y;
-		while (x <= 1000) {
-			System.out.println(x);
-			x = y;
-			y = z;
-			z = x + y;
-
+		int n1 = 1;
+		int n2 = 2;
+		int n3 = n1 + n2;
+		while (n1 <= 1000) {
+			System.out.println(n1);
+			n1 = n2;
+			n2 = n3;
+			n3 = n1 + n2;
 		}
 	}
 }
