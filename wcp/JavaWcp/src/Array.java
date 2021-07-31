2ef15d86288b04e8742b85d94572ce8ce613a50f
Fikutaro <nomio9029@gmail.com>
2021-07-31 17:10:56 +0900

    practice array

--- wcp/JavaWcp/src/Array.java
+++ wcp/JavaWcp/src/Array.java
@@ -1,18 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 public class Array {
-public static void main(String[] args)throws IOException {
-	BufferedReader br = new BufferedReader(
-			new InputStreamReader(System.in));
-	int array[] = new int[10];
-	
-	for (int i = 0; i < array.length; i++)
-	array[i] = Integer.parseInt(br.readLine());
-	
-	System.out.println();
-	
-	for( int i = 0 ; i < 10 ; i++ )
-	    System.out.println( array[i] * 2 );
-}
+	public static void main(String[] args) {
+
+		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i] * 2);
+		}
+		System.out.println();
+
+		for (int i = 9; i >= 0; i--) {
+			System.out.println(array[i]);
+
+		}
+
+		System.out.print("偶数:");
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] % 2 == 0) {
+
+				System.out.print(" " + array[i]);
+			}
+		}
+		
+		System.out.println();
+		System.out.print("奇数:");
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] % 2 == 1) {
+
+				System.out.print(" " + array[i]);
+			}
+		}
+	}
 }
