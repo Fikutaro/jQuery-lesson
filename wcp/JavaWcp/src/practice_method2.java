1296c970f2bfa0553ca94d2325bd3e2867d89cfe
Fikutaro <nomio9029@gmail.com>
2021-08-04 17:13:04 +0900

    practice method2

--- wcp/JavaWcp/src/practice_method2.java
+++ wcp/JavaWcp/src/practice_method2.java
@@ -2,6 +2,7 @@
 public class practice_method2 {
 public static void main(String[] args){
 	pyramid(5,'a');
+	kuku(9);
 	
 } 
 
@@ -15,4 +16,9 @@ static void pyramid(int n, char ch) {
 }
 
 
+static void kuku(int n) {
+	for (int i = 1; i <= 9; i++) {
+		System.out.println(n + "*" + i + "=" + (n*i));
+	}
+}
 }
